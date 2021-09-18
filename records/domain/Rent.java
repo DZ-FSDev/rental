5a6e23161e0f11ef4645f0120950acbaaabf90c3
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-09-18 14:25:05 -0500

    Update Rent.java

--- records/domain/Rent.java
+++ records/domain/Rent.java
@@ -12,6 +12,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,6 +21,7 @@ import lombok.Setter;
  * Basic domain object representing rent to be paid by a tenant.
  * 
  * @author DZ-FSDev
+ * @since 16
  * @version 0.0.1
  */
 @Entity
@@ -26,26 +29,30 @@ public class Rent implements Serializable {
 	private static final long serialVersionUID = 7437522577109015377L;
 	
 	private @Getter @Setter @GeneratedValue @Id Long rentId;
-	private @Getter @Setter @Column(scale = 2, nullable = false, updatable = false) BigDecimal amount;
-	private @Getter @Setter @Column(nullable = false, updatable = false) long effectiveStart;
-	private @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL, mappedBy = "rent") List<Contract> contracts;
+	private @Getter @Setter @Column(scale = 2, nullable = false, updatable = false, unique = true) BigDecimal amount;
+	private @Getter @Setter @JsonIgnore @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "rent") List<Contract> contracts;
 	
 	/**
-	 * Zero-arg constructor for the Rent domain object
+	 * Default constructor for the Rent domain object
 	 */
 	public Rent() {}
 
+	/**
+	 * @since 0.0.1
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 577;
 		int result = 1;
 		result = prime * result + ((amount == null) ? 0 : amount.hashCode());
 		result = prime * result + ((contracts == null) ? 0 : contracts.hashCode());
-		result = prime * result + (int) (effectiveStart ^ (effectiveStart >>> 32));
 		result = prime * result + ((rentId == null) ? 0 : rentId.hashCode());
 		return result;
 	}
 
+	/**
+	 * @since 0.0.1
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -62,8 +69,6 @@ public class Rent implements Serializable {
 			if (other.contracts != null)
 				return false;
 		} else if (!contracts.equals(other.contracts))
-			return false;
-		if (effectiveStart != other.effectiveStart)
 			return false;
 		if (rentId == null) {
 			if (other.rentId != null)
